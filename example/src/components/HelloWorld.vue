0151ea8bd54b25cd1e62fd9c72d45359a475a783
ChiefOfGxBxL <langgb@clarkson.edu>
2023-05-01 22:58:24 -0400

    Add CSS classes as an option for styling tokens

--- example/src/components/HelloWorld.vue
+++ example/src/components/HelloWorld.vue
@@ -37,8 +37,8 @@
           <input v-model="customHighlight"  v-on:keyup.13="handleNewHighlights"/>
           <ul>
               <li v-for="(h,i) in this.highlight" :key="i">
-                <span v-if="h.start && h.end">Range: <span :style="h.style || defaultStyle">{{h.text || h}} </span> </span>
-                <span v-else>Text: <span :style="h.style || defaultStyle">{{(h.text || h).toString()}}</span> </span>
+                <span v-if="h.start && h.end">Range: <span :class="h.classList || defaultClassList" :style="h.style || defaultStyle">{{h.text || h}} </span> </span>
+                <span v-else>Text: <span :class="h.classList || defaultClassList" :style="h.style || defaultStyle">{{(h.text || h).toString()}}</span> </span>
               </li>
           </ul>
 
@@ -47,6 +47,9 @@
 
           <h3> Source </h3>
           <a class="npminstall" href="https://github.com/SyedWasiHaider/vue-highlightable-input">https://github.com/SyedWasiHaider/vue-highlightable-input</a>
+
+          <h3> Icons </h3>
+          <p>Icons provided for free by <a href="https://icons8.com/" target="_blank">Icons8</a></p>
       </v-container>
     </v-app>
 </template>
@@ -64,9 +67,14 @@ export default {
   },
   data() {
     return {
-      msg: 'A bold new future hacker news',
-      defaultStyle: { 'background-color' : 'purple', 'color' : 'white' },
+      msg: 'When we go out to restaurants (#restaurant-life) together, we like to order tasty food. I order a hamburger, and they order pizza and taccos. Whoops, we made a typo (and typo is always underlined!). I meant to type tacos. Also, the word Hamburger is CASEsensitive, so HAMBURGER will not trigger the styling. Pizza is good for parties. #goodfood #parties #latenight',
+      defaultClassList: [],
+      defaultStyle: { 'background-color' : 'gray', 'color' : 'white' },
       highlight: [
+        { text: 'hamburger', classList: ['icon', 'hamburger'], caseSensitive: true },
+        { text: 'pizza', classList: ['icon', 'pizza'] },
+        { text: 'taco', classList: ['icon', 'taco'] },
+        { text: 'typo', style: "border-bottom: 2px SOLID red" },
         {text:'hacker news', style:"background-color:#ff6600"},
         {text:'CASEsensitive', style:"background-color:#fca88f", caseSensitive: true},
         {text:'@Soup', style:"background-color:#bbe4cb"},
@@ -76,6 +84,7 @@ export default {
         {start:0, end:1, style:"border: 3px solid #AAAD21;"},
         {start:3, end:5, style:"border: 2px solid #73AD21;"},
         {text: /[+]?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}/img, style:"color: white; background-color:#aa2313"}, // Phone number regex
+        { text: /#[\w-]+/ig, style: "font-size:0.7em; border:1px SOLID #000; padding:1px; background-color:white;"}, // Hash tag
         { text: /{{\w+}}/gm, style: 'color: green;' },
         { text: /{{.*?}}/gm, style: 'color: red' },
         { text: /{{\w+/gm, style: 'color: green' },
@@ -105,7 +114,8 @@ h3 {
 }
 
 li {
-  text-align: left
+  text-align: left;
+  margin-top: 5px;
 }
 
 a {
@@ -116,7 +126,8 @@ a {
   height: 40px;
   width: auto;
   margin: 30px;
-  background-color: #e2e1ee
+  background-color: #e2e1ee;
+  padding: 20px;
 }
 
 .npminstall {
@@ -134,11 +145,29 @@ a {
 }
 
 #container {
-    width: 640px; /*can be in percentage also.*/
-    height: auto;
-    margin: 0 auto;
-    padding: 10px;
-    position: relative;
+  width: 640px;
+  height: auto;
+  margin: 0 auto;
+  padding: 10px;
+  position: relative;
 }
+</style>
 
+<style>
+.icon {
+  background-size: contain;
+  background-repeat: no-repeat;
+  padding: 2px 2px 2px 25px;
+  border: 1px SOLID #000;
+  border-radius: 4px;
+}
+.hamburger {
+  background-image: url('../assets/icons8-hamburger-30.png');
+}
+.pizza {
+  background-image: url('../assets/icons8-pizza-30.png');
+}
+.taco {
+  background-image: url('../assets/icons8-taco-30.png');
+}
 </style>
